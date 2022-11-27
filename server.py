d5b3b8126b090d4b0b4670f94699d781e5dcfadd
Mofe_Sanya <86200511+tioluwanimofe@users.noreply.github.com>
2022-11-27 13:16:23 -0600

    Add files via upload
    Initial commit

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, url_for, redirect
+
+app = Flask(__name__)
+print(__name__)
+
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+
+@app.route('/<username>')
+def always_send_back(username=None):
+    # show the user profile for that user
+    return render_template("index.html")
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect("./success.html")
+    else:
+        return redirect('./error.html')
